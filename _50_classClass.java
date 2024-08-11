9fccdc0abd18c3eb4e6fb5d9a34044e7d7b86edf
Shub-ev <shubhamg200204@gmail.com>
2024-08-11 11:17:51 +0530

    A

--- _50_classClass.java
+++ _50_classClass.java
@@ -1,13 +1,21 @@
-
 import java.lang.reflect.Method;
 
 public class _50_classClass{
     public static void main(String[] args) throws ClassNotFoundException {
-        Class c = Class.forName("Student").getClass();
+        Class c = Class.forName("Student");
         Method[] arr = c.getDeclaredMethods();
         for(Method m : arr){
             System.out.println(m.getName());
         }
+
+        Student s1 = new Student();
+        Class c1 = s1.getClass();
+        Student s2 = new Student();
+        Class c2 = s2.getClass();
+
+        System.out.println(c1 == c2);
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
     }
 }
 
