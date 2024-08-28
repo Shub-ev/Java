b63e9e6f2d4f42cffd578a873cb50712b0496f63
shubhamg <shubhamg200204@gmail.com>
2024-08-28 19:17:31 +0530

    a

---
+++ _06_arithmatic_ope.java
@@ -0,0 +1,15 @@
+public class _06_arithmatic_ope {
+    public static void main(String args[]){
+        // ******  arithmatic operators  ******
+
+        int a = 10;
+        int b = 4;
+
+
+        System.out.println(a + b);  //  +  addition
+        System.out.println(a - b);  //  -  subtraction
+        System.out.println(a * b);  //  *  multiplication
+        System.out.println(a / b);  //  /  division
+        System.out.println(a % b);  //  &  modulo
+    }
+}
---
+++ _07_assignment_oper.java
@@ -0,0 +1,17 @@
+public class _07_assignment_oper {
+    public static void main(String args[]){
+        // ******  Assignment operators  ******
+
+        // A. Single Assignment Operator
+        int a = 10; // this is simple assignment operator
+        System.out.println(a);
+
+
+        // B. Compund Assignment Operator
+        System.out.println(a += 4); // a = a + 4
+        System.out.println(a -= 4); // a = a - 4
+        System.out.println(a *= 4); // a = a * 4
+        System.out.println(a /= 4); // a = a + 4
+        System.out.println(a %= 4); // a = a % 4
+    }
+}
