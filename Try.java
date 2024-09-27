45ebf5e5893b61a0ca18baff1ab7f52ff466bbfe
shubhamg <shubhamg200204@gmail.com>
2024-09-27 23:31:10 +0530

    a

--- Try.java
+++ Try.java
@@ -1,9 +1,8 @@
 class Try {
     public static void main(String args[]) {
-        byte b = -1;
-        for(int i = 0; i < 8; i++){
-            System.out.println(b);
-            b = (byte) (b >> 1);
+        int a = 10;
+        if(a){
+            System.out.println("Hello");
         }
     }
-}
+} 
