171689e5ebfbfd21d3ee53ac7743a8f8832223f0
shubhamg <shubhamg200204@gmail.com>
2024-10-06 21:54:35 +0530

    a

--- Try.java
+++ Try.java
@@ -1,8 +1,29 @@
-class Try {
-    public static void main(String args[]) {
-        int a = 10;
-        if(a){
-            System.out.println("Hello");
-        }
+class A{
+    int a;
+
+    A(){
+        System.out.println("Inside Constructor!");
     }
-} 
+
+    {
+        System.out.println("Inside block!");
+    }
+
+    static {
+        System.out.println("Inside Static block!");
+    }
+
+    A(int a){
+        this.a = a;
+    }
+}
+
+class Try{
+    public static void main(String args[]){
+
+        A a = new A();
+        A b = new A();
+
+        System.out.println(a.a);
+    }
+}
