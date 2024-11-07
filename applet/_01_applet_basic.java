00bc9f02e2eec47bc306659deb6936e60e6f7f8a
Shub-ev <shubhamg200204@gmail.com>
2024-11-07 19:19:31 +0530

    a

---
+++ applet/_01_applet_basic.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.applet.*;
+
+public class _01_applet_basic extends Applet{
+    public void paint(Graphics g) {
+        g.drawString("A Simple String", 20, 20);
+    } 
+}
